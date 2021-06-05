c8ca19b30095d37df37839d90eae4861d3ed7b0e
Miao Cai <philliphily@gmail.com>
2021-06-05 22:57:28 +0800

    chore: update mass-ts to 1.0.5

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/matrix-profile-foundation/mass_ts',
-    version='0.1.4',
+    version='0.1.5',
     zip_safe=False,
 )
